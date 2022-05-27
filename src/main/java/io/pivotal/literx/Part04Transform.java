fbb4dda8b37c4fa7cfce72df47dadbd8031ddaf6
Eslashier <anfelov@gmail.com>
2022-05-26 23:22:48 -0500

    Part05Merge and Part06Request

--- src/main/java/io/pivotal/literx/Part04Transform.java
+++ src/main/java/io/pivotal/literx/Part04Transform.java
@@ -24,14 +24,16 @@ public class Part04Transform {
 
 	// TODO Capitalize the users username, firstName and lastName
 	Flux<User> capitalizeMany(Flux<User> flux) {
-		return null;
+		return flux.map(user -> new User(user.getUsername().toUpperCase(),
+				user.getFirstname().toUpperCase(),
+				user.getLastname().toUpperCase()));
 	}
 
 //========================================================================================
 
 	// TODO Capitalize the users username, firstName and lastName using #asyncCapitalizeUser
 	Flux<User> asyncCapitalizeMany(Flux<User> flux) {
-		return null;
+		return flux.flatMap(this::asyncCapitalizeUser);
 	}
 
 	Mono<User> asyncCapitalizeUser(User u) {
